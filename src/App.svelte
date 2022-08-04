f1a06e7ce2e882ba830113195c84ba2fd1ebf1cb
linjoe2 <linjoe_is@live.nl>
2022-08-04 15:22:16 +0200

    startup

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+ import Menu from "./components/menu.svelte";
+ import Scene1 from "./frames/scene1.svelte";
+ import Scene2 from "./frames/scene2.svelte";
+ import Scene3 from "./frames/scene3.svelte";
+ import Scene4 from "./frames/scene4.svelte";
+ import Scene5 from "./frames/scene5.svelte";
+ import Scene6 from "./frames/scene6.svelte";
+</script>
+
+<main>
+ <Menu />
+
+    <Scene1 />
+    <Scene2 />
+    <Scene3 />
+    <Scene4 />
+    <Scene5 />
+    <Scene6 />
+
+
+</main>
+
+<style>
+ 
+</style>
---
+++ src/components/menu.svelte
@@ -0,0 +1,55 @@
+<script>
+import MdMenu from 'svelte-icons/md/MdMenu.svelte'
+import MdClose from 'svelte-icons/md/MdClose.svelte'
+
+let menuOpen = false;
+
+let MenuToggle = () => { 
+    menuOpen = !menuOpen
+  }
+
+</script>
+
+<style>
+  .menu-icon {
+    color: black;
+width: 32px;
+height: 32px;
+margin: 15px;
+position: absolute;
+right: 0px;
+z-index: 5;
+cursor: pointer;
+  }
+
+  .menu{
+
+  }
+
+  li {
+
+  }
+  a {
+    cursor: pointer;
+    color: black;
+  }
+</style>
+
+<div class="menu-icon" on:click={MenuToggle}>
+    {#if menuOpen}
+        <MdClose />
+    {:else}
+        <MdMenu />
+    {/if}
+
+    {#if menuOpen}
+    <div class="menu">
+        <nav>
+            <li><a>Home</a></li>
+            <li><a>About us</a></li>
+        </nav>
+    </div>
+    {/if}
+</div>
+
+
